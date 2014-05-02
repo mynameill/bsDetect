3fb3db146d66f86dc02905305ea846e2fa71a65a
Redcamel <webseon@gmail.com>
2014-05-02 12:40:34 +0900

    detect.0.4.js - 모바일에서 모든 파이폭스로 체크되는거 변경 detect.0.5.js - webGL 디텍터 초안 업로드 bs.detect.gpu.html - 테스트 케이스 작성

--- detect.0.5.js
+++ detect.0.5.js
@@ -1,7 +1,7 @@
 var bs = bs || {};
 
 ( function(){
-    var detectWindow, detectDOM;
+    var detectWindow, detectDOM, detectGPU;
     detectWindow = function( W, detect ){
         var navi = W['navigator'], agent = navi.userAgent.toLowerCase(),
             platform = navi.platform.toLowerCase(),
@@ -121,8 +121,32 @@ var bs = bs || {};
         } ) if( t0.hasOwnProperty(k) ) detect[k] = t0[k];
         return detect;
     };
+    detectGPU = function( W, detect ){
+        if( !detect ) detect = {};
+        var canvas = document.createElement('canvas')
+        var gl=canvas.getContext('webgl')||canvas.getContext("experimental-webgl")||canvas.getContext("webkit-3d")||canvas.getContext("moz-webgl")
+        var gpu = {enable : gl ? 1 : 0,contextAttributes: {}}
+        function infoFunc($t, $t2) {
+            $t2 ? $t2=gpu[$t2]={} : $t2=gpu
+            for(var i=0, l=$t.length; i < l; i++) $t2[$t[i]]=gl.getParameter(gl[$t[i]])
+        }
+        function glTest($gl) {
+            var t=$gl.getContextAttributes();
+            for(var i in t) gpu.contextAttributes[i]=t[i]
+            t=['VENDOR', 'VERSION', 'SHADING_LANGUAGE_VERSION', 'RENDERER'], infoFunc(t)
+            t=['MAX_VERTEX_ATTRIBS', 'MAX_VARYING_VECTORS', 'MAX_VERTEX_UNIFORM_VECTORS', 'MAX_VERTEX_TEXTURE_IMAGE_UNITS'], infoFunc(t, 'vertex')
+            t=['MAX_FRAGMENT_UNIFORM_VECTORS'], infoFunc(t, 'fragment')
+            t=['MAX_TEXTURE_SIZE', 'MAX_CUBE_MAP_TEXTURE_SIZE', 'MAX_COMBINED_TEXTURE_IMAGE_UNITS', 'MAX_TEXTURE_IMAGE_UNITS'], infoFunc(t, 'texture')
+            t=['MAX_RENDERBUFFER_SIZE', 'MAX_VIEWPORT_DIMS', 'RED_BITS', 'GREEN_BITS', 'BLUE_BITS', 'ALPHA_BITS', 'DEPTH_BITS', 'STENCIL_BITS'], infoFunc(t, 'renderBuffer')
+        }
+        gpu.enable ? glTest(gl) : null;
+        detect.gpu =gpu
+        console.log(detect)
+        return detect;
+    }
 
     // @test
     bs.detectWindow = detectWindow;
     bs.detectDOM = detectDOM;
+    bs.detectGPU = detectGPU;
 } )();
